293be45badd470106954fd73e09461395b9f2aa6
Ainz OowlGown <tonienm07@gmail.com>
2026-02-02 12:14:03 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14B8A6"
+      }
+    }
+  },
+  plugins: []
+};
