e55bd92d4e44deb7c2737036cde6587b45571164
Ainz OowlGown <tonienm07@gmail.com>
2026-02-03 02:00:28 +0300

    Delete src directory

--- src/pages/portal/StudentPortal.tsx
+++
@@ -1,8 +0,0 @@
-export default function StudentPortal() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Student Portal</h1>
-      <p>View attendance, grades, timetable, and reports.</p>
-    </div>
-  );
-}
